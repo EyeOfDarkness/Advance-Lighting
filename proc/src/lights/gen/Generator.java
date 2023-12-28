6ce5d9be85235fc9a3e91cb82a1696956c8d68a8
GlennFolker <glenn.folk08@gmail.com>
2023-12-28 16:52:38 +0700

    Automate base unit light regions

--- proc/src/lights/gen/Generator.java
+++ proc/src/lights/gen/Generator.java
@@ -4,29 +4,28 @@ import arc.*;
 import arc.files.*;
 import arc.graphics.*;
 import arc.mock.*;
+import arc.struct.*;
 import arc.util.*;
-import arc.util.Log.*;
-import lights.gen.GenAtlas.*;
+import lights.gen.LightsAtlas.*;
 import mindustry.async.*;
 import mindustry.core.*;
 import mindustry.mod.*;
+
+import java.util.concurrent.*;
 
 import static mindustry.Vars.*;
 import static arc.Core.*;
 
 public class Generator {
-    public static GenAtlas atlas;
+    public static LightsAtlas atlas;
 
     private final TaskQueue runs = new TaskQueue();
-    private final Fi fetchedDir;
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception{
         new Generator(Fi.get(args[0]));
     }
 
-    private Generator(Fi fetchedDir){
-        this.fetchedDir = fetchedDir;
-
+    private Generator(Fi fetchedDir) throws Exception{
         try{
             ArcNativesLoader.load();
         }catch(Throwable ignored){}
@@ -40,7 +39,7 @@ public class Generator {
         };
         files = new MockFiles();
         settings = new Settings();
-        Core.atlas = atlas = new GenAtlas();
+        Core.atlas = atlas = new LightsAtlas();
 
         asyncCore = new AsyncCore();
         state = new GameState();
@@ -48,27 +47,83 @@ public class Generator {
 
         content = new ContentLoader();
         content.createBaseContent();
-
         runs.run();
-        addFetched();
+
+        var mask = IntSet.with(
+            // [Neoplasm]:
+            // Neoplasm orange.
+            0x9e172cff, 0xe05438ff, 0xf98f4aff,
+            // [Serpulo]:
+            // Core yellow.
+            0xd4816bff, 0xffd37fff,
+            // Offense orange.
+            0xd06b53ff, 0xffa665ff,
+            // Support green.
+            0x62ae7fff, 0x84f491ff,
+            // Specialist purple.
+            0x665c9fff, 0xbf92f9ff,
+            // [Erekir]:
+            // Defense light blue.
+            0x8ca9e8ff, 0xd1efffff,
+            // Offense brown.
+            0xbc5452ff, 0xfeb380ff
+        );
+
+        var exec = Threads.executor("Vanilla-Sprite-Processor", OS.cores);
+        Queue<Future<Runnable>> tasks = new Queue<>();
+        fetchedDir.walk(file -> {
+            if(file.extEquals("png")){
+                tasks.addLast(exec.submit(() -> {
+                    var name = file.nameWithoutExtension();
+                    var relative = file.parent().absolutePath().substring(fetchedDir.absolutePath().length());
+                    if(relative.startsWith("/")) relative = relative.substring(1);
+
+                    var image = new Pixmap(file);
+
+                    boolean found = false;
+                    for(int x = 0, width = image.width; x < width; x++){
+                        for(int y = 0, height = image.height; y < height; y++){
+                            int pixel = image.getRaw(x, y);
+                            if(mask.contains(pixel)){
+                                found = true;
+                            }else{
+                                image.setRaw(x, y, (Color.blackRgba & 0xffffff00) | Color.ai(pixel));
+                            }
+                        }
+                    }
+
+                    if(found){
+                        var rel = relative;
+                        return () -> atlas.addRegion(name, new LightsRegion(name, rel, image));
+                    }else{
+                        image.dispose();
+                        return () -> {};
+                    }
+                }));
+            }
+        });
+
+        while(!tasks.isEmpty()){
+            var task = tasks.removeFirst();
+            task.get().run();
+        }
 
         content.init();
         runs.run();
         content.load();
         runs.run();
 
-        atlas.dispose();
-    }
-
-    private void addFetched(){
-        fetchedDir.walk(file -> {
-            if(file.extEquals("png")){
-                var name = file.nameWithoutExtension();
-                var relative = file.parent().absolutePath().substring(fetchedDir.absolutePath().length());
-                if(relative.startsWith("/")) relative = relative.substring(1);
-
-                atlas.addRegion(name, new GenRegion(name, relative, new Pixmap(file)));
+        atlas.each(reg -> {
+            var image = reg.pixmap();
+            for(int x = 0, width = image.width; x < width; x++){
+                for(int y = 0, height = image.height; y < height; y++){
+                    if(!mask.contains(image.getRaw(x, y))){
+                        image.setRaw(x, y, Color.clearRgba);
+                    }
+                }
             }
         });
+        atlas.save();
+        atlas.dispose();
     }
 }
--- proc/src/lights/gen/LightsAtlas.java
+++ proc/src/lights/gen/LightsAtlas.java
@@ -1,55 +1,64 @@
 package lights.gen;
 
 import arc.files.*;
+import arc.func.*;
 import arc.graphics.*;
 import arc.graphics.g2d.*;
 import arc.struct.*;
+import arc.util.*;
+import lights.gen.LightsAtlas.*;
 
 import static lights.gen.Generator.*;
 
-public class GenAtlas extends TextureAtlas{
-    public GenRegion clear;
-    private final OrderedMap<String, GenRegion> regions = new OrderedMap<>();
+public class LightsAtlas extends TextureAtlas implements Eachable<LightsRegion>{
+    public LightsRegion clear;
+    private final OrderedMap<String, LightsRegion> regions = new OrderedMap<>();
 
-    public GenAtlas(){
+    public LightsAtlas(){
         regions.orderedKeys().ordered = false;
-        clear = addRegion("clear", new GenRegion("clear", ".", Pixmaps.blankPixmap()));
+        clear = addRegion("clear", new LightsRegion("clear", ".", Pixmaps.blankPixmap()));
     }
 
     @Override
-    public GenRegion addRegion(String name, TextureRegion textureRegion){
-        GenRegion reg = (GenRegion)textureRegion;
+    public LightsRegion addRegion(String name, TextureRegion textureRegion){
+        if(!(textureRegion instanceof LightsRegion reg)){
+            throw new IllegalArgumentException("Must be LightsRegion");
+        }
 
         regions.put(name, reg);
         return reg;
     }
 
     @Override
-    public GenRegion addRegion(String name, Texture texture, int x, int y, int width, int height){
+    public LightsRegion addRegion(String name, Texture texture, int x, int y, int width, int height){
         Pixmap pixmap = texture.getTextureData().getPixmap();
-        GenRegion reg = new GenRegion(name, ".", Pixmaps.crop(pixmap, x, y, width, height));
+        LightsRegion reg = new LightsRegion(name, ".", Pixmaps.crop(pixmap, x, y, width, height));
 
         regions.put(name, reg);
         return reg;
     }
 
     @Override
-    public GenRegion find(String name){
+    public LightsRegion find(String name){
         if(!regions.containsKey(name)){
-            return new GenRegion(name, ".", null);
+            return new LightsRegion(name, ".", null);
         }
 
         return regions.get(name);
     }
 
     @Override
-    public GenRegion find(String name, String def){
+    public LightsRegion find(String name, String def){
         return regions.get(name, find(def));
     }
 
     @Override
-    public GenRegion find(String name, TextureRegion def){
-        return regions.get(name, (GenRegion)def);
+    public LightsRegion find(String name, TextureRegion def){
+        if(!(def instanceof LightsRegion reg)){
+            throw new IllegalArgumentException("Must be LightsRegion");
+        }
+
+        return regions.get(name, reg);
     }
 
     @Override
@@ -59,18 +68,31 @@ public class GenAtlas extends TextureAtlas{
 
     @Override
     public void dispose(){
-        for(GenRegion pix : regions.values()){
-            if(pix.pixmap != null) pix.pixmap.dispose();
+        for(var reg : regions.values()){
+            if(reg.pixmap != null) reg.pixmap.dispose();
         }
 
         regions.clear();
     }
 
-    public static class GenRegion extends AtlasRegion{
+    @Override
+    public void each(Cons<? super LightsRegion> cons){
+        for(var reg : regions.values()){
+            cons.get(reg);
+        }
+    }
+
+    public void save(){
+        for(var reg : regions.values()){
+            reg.save(false);
+        }
+    }
+
+    public static class LightsRegion extends AtlasRegion{
         private final String relative;
         private final Pixmap pixmap;
 
-        public GenRegion(String name, String relative, Pixmap pixmap){
+        public LightsRegion(String name, String relative, Pixmap pixmap){
             this.name = name;
             this.pixmap = pixmap;
             this.relative = relative;
